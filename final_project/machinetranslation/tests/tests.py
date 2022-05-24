220636b8ed39de64eb70d0edf40d0e001a57f6ef
mattamu <mattamu.itu@gmail.com>
2022-05-24 12:04:22 +0000

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -10,15 +10,17 @@ class TestEnglishToFrenchTranslation(unittest.TestCase):
     def test_english_to_french_translation(self):
         english_text = "Hello"
         english_to_french_translation = english_to_french(english_text)
-        self.assertEqual("", "") # Test for null input for englishToFrench
+        self.assertEqual("", "")  # Test for null input for englishToFrench
         self.assertEqual(english_to_french_translation, "Bonjour")
+
 
 class TestFrenchToEnglishTranslation(unittest.TestCase):
     def test_french_to_english_translation(self):
         french_text = "Bonjour"
         french_to_english_translation = french_to_english(french_text)
-        self.assertEqual("", "") # Test for null input for frenchToEnglish
+        self.assertEqual("", "")  # Test for null input for frenchToEnglish
         self.assertEqual(french_to_english_translation, "Hello")
+
 
 if __name__ == '__main__':
     unittest.main()
